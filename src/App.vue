52069d6d7026c26b754f93aa0af0905b49d0cf40
maolintuntun <39028815+maolintuntun@users.noreply.github.com>
2021-01-13 14:52:28 +0800

    My first vue.js app
    up

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <test></test>
+  </div>
+</template>
+
+<script>
+//通过import语法，引入刚刚做好的test组件
+import HelloWorld from './components/HelloWorld.vue'
+import test from './components/test.vue'
+
+//引入好了之后，在components内进行局部注册.之后就能在第五行的div里调用test标签了
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    test,
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #3e5974;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,42 @@
+<!-- 这是一个单文件组件 单文件组件分为三个部分 -->
+
+<template>
+  <!-- template放置的是DOM结构，模板，或者是 视图区域 -->
+  <h2 class="red">test...{{ msg }}</h2>
+</template>
+
+<script>
+// script是脚本区域
+// 为这个组件声明这个组件的属性（props)、数据、方法
+export default {
+  name: 'HelloWorld',
+
+  //props是这个组件的属性
+  props: {
+    msg: {
+      //类型
+      type: String,
+      //默认值
+      default: ' test msg',
+    },
+  },
+  //注册这个组件对应的方法
+  methods: {},
+
+  //注册这个组件对应的数据
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style>
+/*style是样式区域,可以声明一些样式*/
+
+/* 例如将这个h2标题的颜色设为红色的*/
+.red {
+  color: #ff0000;
+}
+</style>
+
+<!-- 在定义好了这个组件之后，就可以去app.vue里面，对这个组件进行引入，和注册！！！ -->
